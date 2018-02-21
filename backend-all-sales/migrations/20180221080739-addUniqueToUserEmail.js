61a421cbc4c9811e5ae9a0a864fc4c9d44f0aa7f
Daniel Hook <danielhook@Daniels-MacBook-Pro-2.local>
2018-02-21 00:10:48 -0800

    unique email

---
+++ backend-all-sales/migrations/20180221080739-addUniqueToUserEmail.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.changeColumn(
+      'Users',
+      'email',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.changeColumn(
+      'Users',
+      'email',
+      {
+        type: Sequelize.STRING
+      }
+    )
+  }
+};
--- backend-all-sales/models/users.js
+++ backend-all-sales/models/users.js
@@ -2,39 +2,80 @@
 const crypto = require('crypto')
 const uuid = require('uuid/v1')
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define('Users', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    encryptedPassword: DataTypes.STRING,
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isEmail: true
+      }
+    },
+    encryptedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     authToken: DataTypes.STRING,
     authTokenExpiration: DataTypes.DATE,
     salt: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        Users.hasMany(models.Items, { foreignKey: "userId" })
+  classMethods: {
+    associate: function(models) {
+      Users.hasMany(models.Items, { foreignKey: "userId" })
+    }
+  },
+    setterMethods:{
+      password(value) {
+        if(value) {
+          const salt = uuid();
+          this.setDataValue('salt', salt)
+          const hash = this.encrypt(value);
+          this.setDataValue('encryptedPassword', hash)
+        }
       }
-    }, {
-      instanceMethods: {
-        toJSON()
+    },
+
+    instanceMethods:{
+      toJSON(){
         return {
           id: this.get('id'),
           firstName: this.get('firstName'),
           lastName: this.get('lastName'),
           email: this.get('email'),
           authToken: this.get('authToken'),
-          authTokenExpiration: this.get('authTokenExpiration')
-        },
-        setAuthToken(){
-           const token = uuid()
-           const expiration = new Date()
-           expiration.setMonth(expiration.getMonth() + 1)
-           this.setDataValue('authToken', token)
-           this.setDataValue('authTokenExpiration', expiration)
-        },
+          authTokenExpiration: this.get('authTokenExpiration'),
+        }
       },
+      encrypt(value) {
+        const salt = this.get('salt');
+        return crypto.createHmac('sha512', salt)
+        .update(value)
+        .digest('hex')
+      },
+      setAuthToken() {
+        const token = uuid();
+        const expiration = new Date();
+        expiration.setMonth(expiration.getMonth() + 1)
+        this.setDataValue('authToken', token)
+        this.setDataValue('authTokenExpiration', expiration)
+      },
+      veryifyPassword(unverifiedPassword) {
+        const encryptedUnverifiedPassword = this.encrypt(unverifiedPassword);
+        return encryptedUnverifiedPassword === this.get('encryptedPassword')
+      }
+    },
+    hooks:{
+      beforeCreate: function(user, options) {
+        user.setAuthToken()
+      }
     }
   });
   return Users;
