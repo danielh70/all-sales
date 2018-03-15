54fafad09a7d11b79d6b548388117a56575530e4
Daniel Hook <danhook007@icloud.com>
2018-03-15 09:27:27 -0700

    ...

--- frontend-all-sales/src/presentation/home.js
+++ frontend-all-sales/src/presentation/home.js
@@ -7,9 +7,9 @@ import {
   PageHeader,
   Row
 } from 'react-bootstrap'
-import reduxlogo from '../../images/reduxlogo.svg'
-import reactlogo from '../../images/reactlogo.svg'
-import graphqllogo from '../../images/graphqllogo.svg'
+import reduxlogo from '../../public/images/reduxlogo.svg'
+import reactlogo from '../../public/images/reactlogo.svg'
+import graphqllogo from '../../public/images/graphqllogo.svg'
 import '../App.css';
 
 const mapStateToProps = (store) => {
