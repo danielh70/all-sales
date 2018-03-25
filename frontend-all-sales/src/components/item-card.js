9c52533a8c3d02f036339ba8f4fb636991cf8ad4
Daniel Hook <danhook007@icloud.com>
2018-03-24 21:14:02 -0700

    final

--- frontend-all-sales/src/components/item-card.js
+++ frontend-all-sales/src/components/item-card.js
@@ -11,20 +11,20 @@ class ItemCard extends Component {
   render() {
     const { id, title, image, description } = this.props
       return (
+        <div className="hvr-grow-shadow">
+          <div className=" item-preview shadow cell">
 
-        <div className="hvr-grow-shadow item-preview ">
-
-          <div className="shadow cell">
-            <img src={image} alt="testing"  className="itemPreview responsive-image"/>
-            <h3 id="thumbnail-label">{title}</h3>
-            <p id="description">{description}</p>
-            <p>
-              <Button id={id} bsStyle="primary" disabled>More Info</Button>&nbsp;
-              <Button id={id} onClick={this.props.handleSubmit} bsStyle="default">Add to Cart</Button>
-            </p>
-            <br />
+            <div className="cell">
+              <img src={image} alt="testing"  className="itemPreview responsive-image"/>
+              <h3 id="thumbnail-label">{title}</h3>
+              <p id="description">{description}</p>
+              <p>
+                <Button id={id} bsStyle="primary" disabled>More Info</Button>&nbsp;
+                <Button id={id} onClick={this.props.handleSubmit} bsStyle="default">Add to Cart</Button>
+              </p>
+              <br />
+            </div>
           </div>
-
         </div>
 
     );
