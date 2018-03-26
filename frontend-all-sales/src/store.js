013f9f8b241929017a9bea45625216d200651eec
Daniel Hook <danhook007@icloud.com>
2018-03-25 17:45:41 -0700

    ...

--- frontend-all-sales/src/store.js
+++ frontend-all-sales/src/store.js
@@ -5,15 +5,17 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/index'
 
-// const middleware = [logger, thunk]
-// var initialState = {}
-//
-// export default createStore(
-//     reducer,
-//     initialState,
-//     composeWithDevTools(applyMiddleware(...middleware))
-//   )
+const middleware = [logger, thunk]
+var initialState = {}
 
+export default createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+  )
+
+
+// process.env = dev? push logger : null
 
 //production
-export default createStore(reducer, applyMiddleware(thunk))
+// export default createStore(reducer, applyMiddleware(thunk))
